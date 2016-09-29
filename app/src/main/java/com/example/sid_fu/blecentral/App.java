a27837ecac88b8a6bf0f9830f592fdd723c3c3cb
Administrator <1032324589@qq.com>
2016-09-29 12:59:03 +0800

    add

--- app/src/main/java/com/example/sid_fu/blecentral/App.java
+++ app/src/main/java/com/example/sid_fu/blecentral/App.java
@@ -1,7 +1,6 @@
 package com.example.sid_fu.blecentral;
 
 import android.app.Activity;
-import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 import android.media.AudioFormat;
@@ -70,8 +69,6 @@ public class App extends Application implements TextToSpeech.OnInitListener{
         String device_token = UmengRegistrar.getRegistrationId(this);
         Logger.e(device_token);
         initSound();
-//        soundManager = SoundManager.getInstance();
-//        soundManager.initSounds(this);
         SoundPlayUtils.init(this);
     }
     public void add(String str) {
@@ -79,20 +76,6 @@ public class App extends Application implements TextToSpeech.OnInitListener{
         speak(str);
     }
     public void speak(String text) {
-        //if(!textToSpeech.isSpeaking())
-            //textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH,null);
-    }
-    public static String getCurProcessName(Context context) {
-        int pid = android.os.Process.myPid();
-        ActivityManager activityManager = (ActivityManager) context
-                .getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningAppProcessInfo appProcess : activityManager
-                .getRunningAppProcesses()) {
-            if (appProcess.pid == pid) {
-                return appProcess.processName;
-            }
-        }
-        return null;
     }
     // add Activity
     public void addActivity(Activity activity) {
@@ -113,16 +96,6 @@ public class App extends Application implements TextToSpeech.OnInitListener{
 
     @Override
     public void onInit(int status) {
-//        if (status == TextToSpeech.SUCCESS) {
-//            int result = textToSpeech.setLanguage(Locale.CHINA);
-//            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.ERROR) {
-//                Toast.makeText(this, "数据丢失或语言不支持", Toast.LENGTH_SHORT).show();
-//            }
-//            if (result == TextToSpeech.LANG_AVAILABLE) {
-////                Toast.makeText(this, "支持该语言", Toast.LENGTH_SHORT).show();
-//            }
-////            Toast.makeText(this, "初始化成功", Toast.LENGTH_SHORT).show();
-//        }
     }
     public void initSound() {
         sp = new SoundPool(6, AudioManager.STREAM_MUSIC, 0);
